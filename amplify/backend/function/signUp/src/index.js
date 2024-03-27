1ee3dc26c2fa821c400d54a2efa1fa1e70d29fd0
tasneemtantawy <tasnim0tantawi@gmail.com>
2024-03-27 12:05:03 +0300

    added lambda

--- amplify/backend/function/signUp/src/index.js
+++ amplify/backend/function/signUp/src/index.js
@@ -27,7 +27,7 @@ exports.handler = async (event) => {
         const password = requestBody.password;
         const user =  await getUserFromCognito(username);
 
-        const userExists = await getUserFromDynamoDB(username) !== undefined || await getUserFromCognito(username) !== undefined;
+        const userExists = !!user;
         if (userExists) {
             if(user.UserAttributes.find(attr => attr.Name === 'email_verified').Value === 'true'){
                 return {
