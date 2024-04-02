dfb4e9ec5a5551fc6726b77f726b50e5fa2dcfdf
tasneemtantawy <tasnim0tantawi@gmail.com>
2024-04-02 11:43:10 +0300

    added lambda

--- amplify/backend/function/syncApplications/src/index.js
+++ amplify/backend/function/syncApplications/src/index.js
@@ -13,6 +13,11 @@ exports.handler = async (event) => {
 
         const student = requestBody.Records[0].dynamodb.NewImage;
         const oldStudent = requestBody.Records[0].dynamodb.OldImage;
+        const applicationId = await getApplication(student.studentCPR.S);
+        console.log(applicationId);
+
+
+
 
 
 
@@ -26,3 +31,51 @@ exports.handler = async (event) => {
     }
 
 };
+
+
+async function updateApplication(applicationId, oldStudent, student) {
+    // update studentName, nationalityCategory, familyIncome
+
+const params = {
+    TableName: 'Application-cw7beg2perdtnl7onnneec4jfa-staging',
+    Key: {
+        id: applicationId
+    },
+    UpdateExpression: '',
+}
+
+if (student.studentName.S !== oldStudent.studentName.S) {
+    params.UpdateExpression += 'SET studentName = :studentName, ';
+    params.ExpressionAttributeValues[':studentName'] = student.studentName;
+}
+
+if (student.nationalityCategory.S !== oldStudent.nationalityCategory.S) {
+    params.UpdateExpression += 'SET nationalityCategory = :nationalityCategory, ';
+    params.ExpressionAttributeValues[':nationalityCategory'] = student.nationalityCategory;
+}
+
+if (student.familyIncome.S !== oldStudent.familyIncome.S) {
+    params.UpdateExpression += 'SET familyIncome = :familyIncome, ';
+    params.ExpressionAttributeValues[':familyIncome'] = student.familyIncome;
+}
+
+if (params.UpdateExpression) {
+    params.UpdateExpression = params.UpdateExpression.slice(0, -2);
+    await dynamoDB.update(params).promise();
+}
+
+
+}
+
+async function getApplication(studentCPR) {
+    const params = {
+        TableName: 'Application-cw7beg2perdtnl7onnneec4jfa-staging',
+        IndexName: 'byStudentCPR',
+        KeyConditionExpression: 'studentCPR = :studentCPR',
+        ExpressionAttributeValues: {
+            ':studentCPR': studentCPR
+        }
+    };
+    const application = await dynamoDB.query(params).promise();
+    return application.Items[0];
+}
