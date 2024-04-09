f0aed9cbbe3216f3fe08ce31cdf4617a333219ef
tasneemtantawy <tasnim0tantawi@gmail.com>
2024-04-09 09:08:27 +0300

    added lambda

--- amplify/backend/function/exportEligibleCsv/src/index.js
+++ amplify/backend/function/exportEligibleCsv/src/index.js
@@ -15,12 +15,13 @@ exports.handler = async (event) => {
     const batchValue = parseInt(event.queryStringParameters?.batch) || new Date().getFullYear();
     const exceptionUniversities = await getExceptionUniversities();
     const extendedUniversities = await getExtendedUniversities();
+    const batchDetails = await getBatchDetails(batchValue);
 
     console.log('Universities:', exceptionUniversities);
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
     try {
-        const applications = await getApplications('Application-cw7beg2perdtnl7onnneec4jfa-staging', batchValue, exceptionUniversities);
+        const applications = await getApplications('Application-cw7beg2perdtnl7onnneec4jfa-staging', batchValue, exceptionUniversities, batchDetails);
         const csv = convertToCsv(applications);
         const url = await uploadToS3(csv);
         return {
@@ -37,7 +38,7 @@ exports.handler = async (event) => {
     }
 };
 
-async function getApplications(tableName, batchValue, exceptionUniversities, extendedUniversities) {
+async function getApplications(tableName, batchValue, exceptionUniversities, extendedUniversities, batchDetails) {
     // const programs = await getPrograms();
     const params = {
         TableName: tableName,
@@ -65,9 +66,34 @@ async function getApplications(tableName, batchValue, exceptionUniversities, ext
     // Remove the NOT_COMPLETED application unless the university is an exception]
 
     console.log('Universities:', exceptionUniversities);
-    allApplications = allApplications.filter(
-        application => application.status !== 'NOT_COMPLETED'
-            || exceptionUniversities.some(async university => university.id === application.universityID) || extendedUniversities.some(async university => university.id === application.universityID));
+
+    allApplications = allApplications.filter(application => {
+        // Filter out rejected applications
+        if (application.status === 'REJECTED') {
+            return false;
+        }
+        // Filter out not completed applications unless they are from exception or extended universities
+        if (application.status === 'NOT_COMPLETED') {
+            // return exceptionUniversities.some(university => university.id === application.universityID)
+            //     || extendedUniversities.some(university => university.id === application.universityID);
+            if(exceptionUniversities.some(university => university.id === application.universityID)){
+                return true;
+            }
+            if(extendedUniversities.some(university => university.id === application.universityID)){
+               // check if today is before the extended deadline
+                const today = new Date();
+                const chosenUniversity = extendedUniversities.find(university => university.id === application.universityID);
+                // TODO: deadline is in days, convert it to a date
+                let deadline = new Date(batchDetails.updateApplicationEndDate);
+                deadline.setDate(deadline.getDate() + chosenUniversity.extensionDuration);
+                return today <= deadline;
+            }
+
+        }
+        // Keep all other applications
+        return true;
+    });
+
 
     return allApplications;
 }
@@ -141,6 +167,18 @@ async function getExtendedUniversities() {
     } while (params.ExclusiveStartKey);
 
     return allUniversities;
+}
+
+async function getBatchDetails(batch) {
+    const params = {
+        TableName: 'Batch-cw7beg2perdtnl7onnneec4jfa-staging',
+        Key: {
+            batch: batch
+        }
+    };
+
+    const batchDetails = await dynamoDB.get(params).promise();
+    return batchDetails.Item;
 }
 
 // async function getPrograms() {
