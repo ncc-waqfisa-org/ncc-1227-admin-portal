2c535c2261d6dc1b15d7506adfadc3f9cb380dbe
tasneemtantawy <tasnim0tantawi@gmail.com>
2024-06-26 14:53:55 +0300

    revert

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -60,6 +60,13 @@ export type AmplifyDependentResourcesAttributes = {
       "Name": "string",
       "Region": "string"
     },
+    "exportUniversityEmails": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "getApplications": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
